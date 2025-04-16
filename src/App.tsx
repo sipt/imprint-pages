a373020070b53df984474d24856ff3481e9ded3c
sipt <wxysipt@gmail.com>
2025-04-16 23:24:47 +0800

    link app store

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,12 @@ function App() {
             <p className="md:text-xl">
               还在为旅行前的繁琐准备而烦恼？让企途来帮你！企途是一款专注于旅行行程规划的智能工具，助你轻松规划每一次精彩旅程。
             </p>
-            <div className="max-w-[700px] mt-10 ">
+            <a
+              className="max-w-[700px] mt-10 "
+              href="https://apps.apple.com/cn/app/%E4%BC%81%E9%80%94-%E8%A1%8C%E7%A8%8B%E8%A7%84%E5%88%92/id6739816753"
+            >
               <img src={apple} alt="screen short" />
-            </div>
+            </a>
           </div>
           {/* </div> */}
         </div>
